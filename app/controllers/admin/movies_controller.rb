a7282abc34325e49ddc2b080c7113ad8d2a13164
takehana0318 <kekesaihi@gmail.com>
2024-04-30 09:30:24 +0900

    chapter03

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -2,4 +2,24 @@ class Admin::MoviesController < ApplicationController
     def index
         @movies = Movie.all
     end
+
+    def new
+        @movie = Movie.new
+    end
+
+    def create
+        @movie = Movie.new(movie_params)
+        if @movie.save
+            redirect_to admin_movies_path
+        else
+            flash.now[:notice] = "Failed to Post"
+            redirect_to new_admin_movie_path
+        end
+    end
+
+    private
+
+    def movie_params
+        params.require(:movie).permit(:name, :year, :description, :image_url, :is_showing)
+    end
 end

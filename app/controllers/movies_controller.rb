64a1afa1728cfd41c8525c4f049548f861c8fdbf
takehana0318 <kekesaihi@gmail.com>
2024-04-30 18:51:37 +0900

    chapter06

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,17 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all
+    if params[:is_showing] == "1" then
+      @movies = Movie.where("is_showing": true)
+    elsif params[:is_showing] == "0" then
+      @movies = Movie.where("is_showing": false)
+    else
+      @movies = Movie.all
+    end
+
+    if params[:keyword]
+      @movies = @movies.where("name LIKE? or description LIKE?", 
+                              "%#{params[:keyword]}%", "%#{params[:keyword]}%")
+    end
+
   end
 end
